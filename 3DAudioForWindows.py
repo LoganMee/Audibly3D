6a9b72a43b05cd25b36a56676d1648e7ab6054fa
Logan Mee <117446049+LoganMee@users.noreply.github.com>
2024-05-29 17:16:53 +0100

    Completed Windows Integration
    • Switched from pygame audio to using pycaw and comtypes to interact with windows output devices
    • Removed all redundant code referring to using files
    • Added a close window function which saves the volume levels before opening and reverts audio levels when closing

--- 3DAudioForWindows.py
+++ 3DAudioForWindows.py
@@ -1,6 +1,8 @@
 from tkinter import *
-from tkinter import filedialog
-import pygame, math, os, time, threading
+import math, time, threading
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 
 #mathmatical Functions
 def distanceBetweenPoints(x1,y1,x2,y2):
@@ -15,64 +17,37 @@ def angleBetweenPoints(x1, x2, hypotenuse):
     return angle
 
 
-
 class Audio3DInterface:
 
     def __init__(self):
         self.root = Tk()
         self.root.title("3D Audio")
-        self.root.geometry("700x700")
+        self.root.geometry("525x700")
         
         self.mainFrame = Frame(self.root)
         self.mainFrame.grid(column=0, row=0, padx=10, pady=10)
         self.sideBar = Frame(self.root)
-        self.sideBar.grid(column=1, row=0, padx=10, pady=10)
-        self.controlsFrame = Frame(self.root)
-        self.controlsFrame.grid(column=0, row=1, padx=10, pady=10)
+        self.sideBar.grid(column=0, row=1, padx=10, pady=10)
 
-        self.filePath = ""
-        self.fileName = ""
-        self.playing = False
-        self.paused = False
-
-        pygame.mixer.init()
-        self.channel0 = pygame.mixer.Channel(0) #left1
-        self.channel0.set_volume(0.5, 0.0)
-        self.channel1 = pygame.mixer.Channel(1) #right1
-        self.channel1.set_volume(0.0, 0.5)
-
-        self.playButtonImg = PhotoImage(file="play.png")
-        self.pauseButtonImg = PhotoImage(file="pause.png")
+        self.devices = AudioUtilities.GetSpeakers()
+        self.interface = self.devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.volume = self.interface.QueryInterface(IAudioEndpointVolume)
+        self.leftStartingVolume = self.volume.GetChannelVolumeLevelScalar(0)
+        self.rightStartingVolume = self.volume.GetChannelVolumeLevelScalar(1)
 
         self.mode = IntVar()
 
     
     #################### Subroutines ####################
+    def onClosing(self):
+        self.volume.SetChannelVolumeLevelScalar(0, self.leftStartingVolume, None)
+        self.volume.SetChannelVolumeLevelScalar(1, self.rightStartingVolume, None)
+        self.root.destroy()
+    
     def run(self):
         self.createWidgets()
+        self.root.protocol("WM_DELETE_WINDOW", self.onClosing)
         self.root.mainloop()
-    
-    #Menu subroutines
-    def openFile(self):
-        self.filePath = filedialog.askopenfilename()
-        self.fileName = os.path.basename(self.filePath)
-
-    #Button subroutines
-    def playMusic(self, playbackText):
-        if not self.paused and self.playing == False:
-            self.channel0.play(pygame.mixer.Sound(self.filePath))
-            self.channel1.play(pygame.mixer.Sound(self.filePath))
-            playbackText.config(text=f"Playing: {self.fileName}")
-            self.playing = True
-        else:
-            pygame.mixer.unpause()
-            self.paused = False
-            playbackText.config(text=f"Playing: {self.fileName}")
-
-    def pauseMusic(self, playbackText):
-        pygame.mixer.pause()
-        self.paused = True
-        playbackText.config(text="Paused")
  
     #Canvas subroutines
     def move(self, event, audioSource, radius, canvas, canvasCentre, canvasHeight, canvasWidth):
@@ -94,17 +69,14 @@ class Audio3DInterface:
         angle = 0
 
         while self.mode.get() == 1:
-            if not self.paused:
-                angle += (1/72*math.pi)
-                newX = canvasCentre[0] + (math.sin(angle) * orbitRadius)
-                newY = canvasCentre[1] + (math.cos(angle) * orbitRadius)
+            angle += (1/72*math.pi)
+            newX = canvasCentre[0] + (math.sin(angle) * orbitRadius)
+            newY = canvasCentre[1] + (math.cos(angle) * orbitRadius)
 
-                canvas.moveto(audioSource, newX - radius/2, newY - radius/2)
-                self.volumeChange(newX, newY, canvasCentre)
+            canvas.moveto(audioSource, newX - radius/2, newY - radius/2)
+            self.volumeChange(newX, newY, canvasCentre)
 
-                time.sleep(5/144)
-            else:
-                time.sleep(0.2)
+            time.sleep(5/144)
 
     def volumeChange(self, newX, newY, canvasCentre):
         distance = distanceBetweenPoints(canvasCentre[0], canvasCentre[1], newX, newY)
@@ -121,8 +93,8 @@ class Audio3DInterface:
 
         #print(leftIntensity, rightIntensity)
         
-        self.channel0.set_volume(leftIntensity, 0.0)
-        self.channel1.set_volume(0.0, rightIntensity)
+        self.volume.SetChannelVolumeLevelScalar(0, leftIntensity, None) #Left Channel
+        self.volume.SetChannelVolumeLevelScalar(1, rightIntensity, None) #Right Channel
 
     def createWidgets(self):
         #################### GUI ####################
@@ -143,25 +115,11 @@ class Audio3DInterface:
         menubar = Menu(self.root)
         self.root.config(menu=menubar)    
 
-        fileMenu = Menu(menubar, tearoff=False)
-        menubar.add_cascade(label="File", menu=fileMenu)
-        fileMenu.add_command(label="Open", command=self.openFile)
-
         settingsMenu = Menu(menubar, tearoff=False)
         menubar.add_cascade(label="Settings", menu=settingsMenu, command=self.createSettingsWidgets)
 
-        #Info Bar
-        playbackText = Label(self.controlsFrame, text="No song playing")
-        playbackText.grid(column=0, row=0, columnspan=2)
-
-        #Buttons
-        playButton = Button(self.controlsFrame, image=self.playButtonImg, borderwidth=0, command=lambda:self.playMusic(playbackText))
-        pauseButton = Button(self.controlsFrame, image=self.pauseButtonImg, borderwidth=0, command=lambda:self.pauseMusic(playbackText))
-        playButton.grid(row=1, column=0, padx=7, pady=10)
-        pauseButton.grid(row=1, column=1, padx=7, pady=10)
-
         #Modes
-        self.orbitThread = threading.Thread(target=self.orbitAudio, args=(audioSource, radius, canvas, canvasCentre, 100))
+        self.orbitThread = threading.Thread(target=self.orbitAudio, args=(audioSource, radius, canvas, canvasCentre, 30))
         self.orbitThread.daemon = True
 
         modesLabel = Label(self.sideBar, text="Modes:", width=15)
